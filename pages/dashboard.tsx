c0393a3113e55453ac87e8d4de9e90f536bcdc87
pedrofleandro <pedrofragosoleandro@gmail.com>
2023-08-20 14:02:15 -0300

    Update repository

---
+++ pages/dashboard.tsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
+import { AuthContext } from "../contexts/AuthContext"
+import { setupAPIClient } from "../srvices/api"
+import { api } from "../srvices/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
+
+export default function Dashboard() {
+    const { user, signOut } = useContext(AuthContext)
+
+    useEffect(() => {
+        api.get('/me')
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+    })
+
+    return (
+        <>
+            <h1>Dashboard: {user?.email}</h1>
+
+            <button onClick={signOut}>Sign out</button>
+
+            <Can permissions={['metrics.list']}>
+                <div>Métricas</div>
+            </Can>
+        </>
+    )
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx)
+    const response = await apiClient.get('/me')
+
+
+    return {
+        props: {}
+    }
+})
